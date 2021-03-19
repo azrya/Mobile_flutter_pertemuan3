8ad5a2d52fcd4bd3457ac2d2145572026a07a589
azrya <azryan21@gmail.com>
2021-03-19 15:47:45 +0700

    kuis

--- lib/main.dart
+++ lib/main.dart
@@ -1,107 +1,34 @@
 import 'package:flutter/material.dart';
-import 'Input.dart';
-import 'Result.dart';
-import 'Convert.dart';
-import 'RiwayatKonversi.dart';
+import 'timer_page.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(new MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  _MyAppState createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      showPerformanceOverlay: false,
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  //controller
-  TextEditingController etInput = new TextEditingController();
-
-  //variabel berubah
-  double _inputUser = 0;
-  double _kelvin = 0;
-  double _reamur = 0;
-
-  //mengeset nilai pada dropdown
-  String _newValue = "Kelvin";
-  double _result = 0;
-
-  //Fungsi perhitungan suhu perlu untuk diubah sehingga hanya memproses konversi sesuai
-  //dengan pilihan pengguna.
-  void _konversiSuhu() {
-    setState(() {
-      _inputUser = double.parse(etInput.text);
-      if (_newValue == "Kelvin")
-        _result = _inputUser + 273;
-      else
-        _result = (4 / 5) * _inputUser;
-        //untuk menampilkan hasil riwayat
-        listViewItem.add("$_newValue : $_result");
-    });
-  }
-
-  //buat list
-  var listItem = {"Kelvin", "Reamur"};
-
-  //variable bertipe List<String> (praktikum 2)
-  // ignore: deprecated_member_use
-  List<String> listViewItem = List<String>();
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("Konverter Suhu"),
-          ),
-          body: Container(
-            margin: EdgeInsets.all(8),
-            child: Column(
-              children: [
-                Input(etInput: etInput),
-                //memperluas anak row
-                buildDropdownButton(),
-                Result(
-                  result: _result,
-                ),
-                Convert(konvertHandler: _konversiSuhu),
-                //Riwayat Konversi
-                Container(
-                  margin: EdgeInsets.only(top: 10.0, bottom: 10.0),
-                  child: Text(
-                    "Riwayat Konversi",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
-                  ),
-                ),
-                Expanded(
-                    child: RiwayatKonversi(listViewItem: listViewItem)),
-              ],
-            ),
-          ),
-        ));
-  }
-
-  DropdownButton<String> buildDropdownButton() {
-    return DropdownButton<String>(
-                items: listItem.map((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-                // isi value dengan variabel _newValue.
-                value: _newValue,
-                onChanged: (String changeValue) {
-                  setState(() {
-                    _newValue = changeValue;
-                  });
-                },
-              );
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Stopwatch"),
+      ),
+      body: new Container(
+        child: new TimerPage()
+      ),
+    );
   }
 }
